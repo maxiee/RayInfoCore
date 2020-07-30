f491f1b6df163ac90a78b120614c3d94e5d3430a
Maxiee <maxieewong@gmail.com>
2020-07-30 13:35:59 +0800

    auto create data directory if not exist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import click
 import json
+from pathlib import Path
 
 @click.group()
 def hello():
@@ -16,13 +17,22 @@ def b():
 hello.add_command(a)
 hello.add_command(b)
 
-def loadConfig():
+def load_config():
     with open('./config.json', 'r') as f:
         return json.load(f)
 
 if __name__ == "__main__":
-    config = loadConfig()
+    config = load_config()
 
+    if config == None:
+        print('Config.json load failed')
+        exit(0)
+
+    data_path = config['dataPath']
     print("dataPath = %s" % config['dataPath'])
+
+    Path(data_path) \
+        .expanduser() \
+        .mkdir(parents=True, exist_ok=True)
     
     hello()
