b8f0e61a1e09b50952f882e80ae9da9890235842
Maxiee <maxieewong@gmail.com>
2020-08-14 20:15:44 +0800

    create table Tag and Site and TagSiteRelation

--- ray_info_core/data/db.py
+++ ray_info_core/data/db.py
@@ -9,7 +9,20 @@ class BaseModel(Model):
 class Author(BaseModel):
     name = CharField(max_length=128)
     description = CharField()
+
+class Tag(BaseModel):
+    name = CharField(64)
+
+class Site(BaseModel):
+    name = CharField(max_length=128)
+    url = CharField()
+    description = CharField()
+
+class TagSiteRelation(BaseModel):
+    tag = ForeignKeyField(Tag, backref='site_relations')
+    site = ForeignKeyField(Site, backref='tag_relations')
+
     
 def init_db(config) -> None:
     db.init(config.get_db_path())
-    db.create_tables([Author], safe=True)
+    db.create_tables([Author, Site, Tag, TagSiteRelation], safe=True)
